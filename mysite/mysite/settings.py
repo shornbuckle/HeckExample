97d447e8b19225064e59d6b377fc29e12ff6991f
sh4986 <65291545+shornbuckle@users.noreply.github.com>
2021-03-18 21:01:59 -0400

    Sean Test branch commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 #this is a test
 
+#seanTest branch comment
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
